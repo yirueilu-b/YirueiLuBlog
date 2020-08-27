62a78b1c3508b11e9c21ac381f71a9724b7ca3bf
盧奕叡 <yirueilu.b@gmail.com>
2020-08-27 15:34:50 +0800

    Updates

--- precache-manifest.644511daaa6154c9c34adacfa2919069.js
+++ precache-manifest.644511daaa6154c9c34adacfa2919069.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "54a91cfbdc513641ddeab01edd53c48e",
+    "revision": "8495dff8a1ae5d62a3bf68427334c525",
     "url": "/YirueiLuBlog/index.html"
   },
   {
-    "revision": "90d1f12932d130eb323c",
+    "revision": "a3bd4909ae9a82781939",
     "url": "/YirueiLuBlog/static/css/main.7cc96115.chunk.css"
   },
   {
-    "revision": "57c4463c377ea6742eec",
-    "url": "/YirueiLuBlog/static/js/2.71a8fc5c.chunk.js"
+    "revision": "d2899ac88c2ea6a9b9c2",
+    "url": "/YirueiLuBlog/static/js/2.d31289a0.chunk.js"
   },
   {
     "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
-    "url": "/YirueiLuBlog/static/js/2.71a8fc5c.chunk.js.LICENSE.txt"
+    "url": "/YirueiLuBlog/static/js/2.d31289a0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "90d1f12932d130eb323c",
-    "url": "/YirueiLuBlog/static/js/main.eae30a40.chunk.js"
+    "revision": "a3bd4909ae9a82781939",
+    "url": "/YirueiLuBlog/static/js/main.0e762d2f.chunk.js"
   },
   {
     "revision": "b9dee457e9603ad11efe",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/YirueiLuBlog/precache-manifest.970def3dfded58cf23e4af12a618c35e.js"
+  "/YirueiLuBlog/precache-manifest.644511daaa6154c9c34adacfa2919069.js"
 );
 
 self.addEventListener('message', (event) => {
